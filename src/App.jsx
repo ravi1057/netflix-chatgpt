e81cbd3eae68a9f8691271e292b2cc091332d62c
ravi p <ravi105763@gmail.com>
2025-09-20 18:49:18 +0530

    Created and configured netflix

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import './App.css'
+import React from 'react';
+function App() {
+
+  return <div className='text-2xl font-bold text-green-800'>Hello Ravi How are you </div>
+}
+
+export default App
